3e25e4928133fb862085c3a357be6cb57a4853c7
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-11-06 11:42:43 +0100

    Resolve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,11 @@
 function Footer() {
     return (
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <footer className="">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2024
-                    <a href="https://flowbite.com/" className="hover:underline">
-                        Obiora Igboanusi
-                    </a>
-                    . All Rights Reserved.
+                    © 2024 Obiora Igboanusi . All Rights Reserved.
                 </span>
-            </div>{" "}
+            </div>
         </footer>
     );
 }
