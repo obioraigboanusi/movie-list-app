a3a37ee4a11b0006019137cb955da455c02407ca
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2024-11-06 11:41:38 +0100

    Resolve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    © 2024
+                    <a href="https://flowbite.com/" className="hover:underline">
+                        Obiora Igboanusi
+                    </a>
+                    . All Rights Reserved.
+                </span>
+            </div>{" "}
+        </footer>
+    );
 }
 
 export default Footer;
